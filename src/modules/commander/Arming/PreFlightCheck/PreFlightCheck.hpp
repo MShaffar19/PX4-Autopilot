721c2117d3f85c687ab4d5ce79d760422af015bb
Igor Misic <igy1000mb@gmail.com>
2022-04-25 11:50:28 +0200

    commander/safety: replace safety.msg with Safety class

--- src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.hpp
+++ src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.hpp
@@ -41,11 +41,10 @@
 #pragma once
 
 #include <uORB/uORB.h>
-#include <uORB/topics/safety.h>
 #include <uORB/topics/vehicle_control_mode.h>
 #include <uORB/topics/vehicle_status_flags.h>
 #include <uORB/topics/vehicle_status.h>
-#include <drivers/drv_hrt.h>
+#include "../../Safety.hpp"
 
 typedef bool (*sens_check_func_t)(orb_advert_t *mavlink_log_pub, vehicle_status_s &status, const uint8_t instance,
 				  const bool is_mandatory, bool &report_fail);
@@ -94,8 +93,8 @@ public:
 	};
 
 	static bool preArmCheck(orb_advert_t *mavlink_log_pub, const vehicle_status_flags_s &status_flags,
-				const vehicle_control_mode_s &control_mode,
-				const safety_s &safety, const arm_requirements_t &arm_requirements, vehicle_status_s &status,
+				const vehicle_control_mode_s &control_mode, const bool safety_button_available, const bool safety_off,
+				const arm_requirements_t &arm_requirements, vehicle_status_s &status,
 				bool report_fail = true);
 
 private:
